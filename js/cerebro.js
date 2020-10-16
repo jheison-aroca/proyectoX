5a03a662796f38799d35ef1fedcd1264f5734fc6
Jheison Aroca Chavez <arocachavezj@gmail.com>
2020-10-15 22:33:45 -0500

    aplicamos javascript

---
+++ js/cerebro.js
@@ -0,0 +1,20 @@
+/*function pedirDomicilio() {
+  var nombre = 5;
+  var cancelado = 1;
+  var pedidoTotal = pedido - cancelado;
+
+  var valorTotal = 4000;
+  var numPersonas = 4;
+  var totalPorPersona = valorTotal / numPersonas;
+
+  console.log("por persona " + totalPorPersona);*/
+//alert('por persona '+totalPorPersona)
+//}
+function cliente() {
+  var nombre = "jheison";
+  var apellido = "aroca";
+  var usuario = nombre + " " + apellido;
+  var mensajeBienvenida= "bienvenido: " + usuario + " sigue";
+  console.log(mensajeBienvenida);
+  alert(mensajeBienvenida);
+}
